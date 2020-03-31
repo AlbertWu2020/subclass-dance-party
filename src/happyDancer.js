742a40579104f8671e71a6024bf748bb94e87da9
thehlau <thehlau@gmail.com>
2020-03-30 22:28:53 -0700

    added a new dancer

---
+++ src/happyDancer.js
@@ -0,0 +1,14 @@
+var makeHappyDancer = function (top, left, timeBetweenSteps) {
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<span class="dancer"><img style="width:50px" src="images/bmo.gif"/><p class="dance-king">Dancing King</p></span>');
+  this.oldStep = makeDancer.prototype.step;
+  this.setPosition(top,left);
+};
+
+makeHappyDancer.prototype = Object.create(makeDancer.prototype);
+makeHappyDancer.prototype.constructor = makeHappyDancer;
+
+makeHappyDancer.prototype.step = function() {
+  makeDancer.prototype.step.call(this);
+  this.$node.toggleClass('happy');
+};
