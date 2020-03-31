f498449d239557a37715d4724882b97f874369fd
thehlau <thehlau@gmail.com>
2020-03-30 22:31:31 -0700

    removed some comments

--- src/popDancer.js
+++ src/popDancer.js
@@ -1,11 +1,8 @@
 var makePopDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
-  this.$node = $('<span class="dancer"><img src="images/image (1)-40x60.png"/><p class="dance-king">Dancing King</p></span>')
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
+  this.$node = $('<span class="dancer"><img src="images/image (1)-40x60.png"/><p class="dance-king">Dancing King</p></span>');
 
   this.oldStep = makeDancer.prototype.step;
-  //this.step();
   this.setPosition(top, left);
 };
 
